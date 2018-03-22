f15703f35632a0b750fcb70b77635fe1d53f6911
xiandian <lvpython@gmail.com>
2018-03-22 19:29:16 +0800

    增加帮助和联系我们

--- src/views/help/mail-us.vue
+++ src/views/help/mail-us.vue
@@ -1,5 +1,20 @@
-<style></style>
+<style lang="css">
+@import './css/base.css';
+@import './css/help.css';
+</style>
 <template>
-	<div></div>
+	<div class="contact w">
+		<div class="contact-in">
+			<p>如果您在使用的过程中遇到任何问题或者使用方面的建议，请反馈给我们，帮助我们完善系统。</p>
+			<p>反馈邮箱：<a href="mailto:lqdzj@longquan.org">lqdzj@longquan.org</a></p>
+			<p>反馈格式：</p>
+			<p>一、邮件标题：【问题反馈】-日期-主题 或者 【使用建议】-日期-主题</p>
+			<p>二、请在邮件内容中详细描述您遇到的问题或建议。包括：</p>
+			<p class="ft14"><span>1. </span>页面的url链接地址。</p>
+			<p class="ft14"><span>2. </span>页面截图。</p>
+			<p class="ft14"><span>3. </span>所使用的操作系统和浏览器版本</p>
+			<p class="ft14"><span>4. </span>问题或建议详细描述</p>
+		</div>
+	</div>
 </template>
 <script></script>
