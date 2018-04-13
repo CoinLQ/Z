4a7a6cfd771375f09b425d96f1c2a7b0dd3adb94
xian <1721997636@qq.com>
2018-04-13 21:24:46 +0800

    修改‘home’为中文“首页”，并删除冗余代码。

--- src/libs/util.js
+++ src/libs/util.js
@@ -130,7 +130,7 @@ util.setCurrentPath = function (vm, name) {
     if (currentPathObj && detailPathObj.name === 'home_index') {
         currentPathArr = [
             {
-                title: '首页',//util.handleTitle(vm, util.getRouterObjByName(routers, 'home_index')),
+                title: '首页',
                 path: '',
                 name: 'home_index'
             }
