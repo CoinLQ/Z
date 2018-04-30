0ac024ddebcb2771f95e82a012b4471a8b886b47
swankie <swankie@163.com>
2018-04-30 15:13:03 +0800

    add help

--- src/views/jiaodui/components/help.vue
+++ src/views/jiaodui/components/help.vue
@@ -35,36 +35,47 @@
     justify-content: space-evenly;
     text-align: left;
 }
+.help-guide {
+  position: fixed;
+  top: 120px;
+  font-size: 32px;
+  margin-left: 40px;
+}
 </style>
 
 <template>
-<div class="cover" :class="{hide: toHide}">
-	<div class="content-wrapper">
-    <div class="content">
-		  <div class="v1"  v-if="!withGlyph">
-        <div class="img-wrapper">
-          <img src="../img/keyboard-op1.png">
+<div>
+  <div class='help-guide' @click="openHelp">
+    <Icon type="ios-help">F1 帮助</Icon>
+  </div>
+  <div class="cover" :class="{hide: toHide}">
+    <div class="content-wrapper">
+      <div class="content">
+        <div class="v1"  v-if="!withGlyph">
+          <div class="img-wrapper">
+            <img src="../img/keyboard-op1.png">
+          </div>
+          <div class="text-wrapper">
+            <ul>
+              <li>[F1] 本帮助页面/切换</li>
+              <li>[1-5] 【显示功能】控制切分图片的放大倍率</li>
+              <li>[AWSD] 控制字块的选择</li>
+              <li>[space] 切分框操作模式/切换</li>
+              <li>小技巧：单边调整键自动进入操作模式</li>
+            </ul>
+            <ul>
+              <li>[ shift + [←↑↓→] ] 【操作模式】单边调整膨胀</li>
+              <li>[ alt+ [←↑↓→] ] 【操作模式】单边调整缩小</li>
+              <li>[ ←↑↓→ ]【操作模式】单方向移动切分框</li>
+              <li>[ ctrl + [←↑↓→] ]【操作模式】加速单方向移动切分框</li>
+              <li>[k] 【操作模式】删除当前切分框/切换</li>
+            </ul>
+          </div>
         </div>
-        <div class="text-wrapper">
-          <ul>
-            <li>[F1] 本帮助页面/切换</li>
-            <li>[1-5] 【显示功能】控制切分图片的放大倍率</li>
-            <li>[AWSD] 控制字块的选择</li>
-            <li>[space] 切分框操作模式/切换</li>
-            <li>小技巧：单边调整键自动进入操作模式</li>
-          </ul>
-          <ul>
-            <li>[ shift + [←↑↓→] ] 【操作模式】单边调整膨胀</li>
-            <li>[ alt+ [←↑↓→] ] 【操作模式】单边调整缩小</li>
-            <li>[ ←↑↓→ ]【操作模式】单方向移动切分框</li>
-            <li>[ ctrl + [←↑↓→] ]【操作模式】加速单方向移动切分框</li>
-            <li>[k] 【操作模式】删除当前切分框/切换</li>
-          </ul>
-        </div>
-      </div>
-      <div v-else>
-        <div class="img-wrapper">
-          <img src="../img/keyboard-op2.png">
+        <div v-else>
+          <div class="img-wrapper">
+            <img src="../img/keyboard-op2.png">
+          </div>
         </div>
       </div>
     </div>
@@ -84,7 +95,11 @@ export default {
       withGlyph: true
 		}
 	},
-
+  methods: {
+    openHelp () {
+      this.toHide = false;
+    }
+  },
     mounted: function() {
     	let _this = this;
 
