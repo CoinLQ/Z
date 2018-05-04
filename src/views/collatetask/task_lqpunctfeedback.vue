7623e4fa620069eecf6719813ff46f32c1ae9177
xianbu <dang.xia@qq.com>
2018-05-04 10:03:44 +0800

    校勘反馈、标点反馈的任务列表

---
+++ src/views/collatetask/task_lqpunctfeedback.vue
@@ -0,0 +1,41 @@
+<template>
+<ObtianTaskList :viewRouteName="pathname" :columns="columns"></ObtianTaskList>
+</template>
+
+<script>
+import ObtianTaskList from './obtain_task_list';
+import ButtonWrapper from '../mytask/ButtonWrapper';
+
+export default {
+    components: {ObtianTaskList: ObtianTaskList},
+    data() {
+        return {
+            pathname: 'lqpunctfeedback',
+            columns: [
+                {
+                    title: '龙泉经名',
+                    key: 'lqsutra_name'
+                },
+                {
+                    title: '第几卷',
+                    key: 'reel_no'
+                },
+                {
+                    title: '状态',
+                    key: 'status'
+                },
+                {
+                    title: '反馈用户',
+                    key: 'fb_user_display'
+                },
+                {
+                    title: '反馈时间',
+                    key: 'created_at'
+                },
+
+            ],
+        }
+    }
+
+}
+</script>
