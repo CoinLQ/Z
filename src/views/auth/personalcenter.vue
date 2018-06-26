2f812e88f724c41dc61f71503b4e60c7c08d4f63
xian <1721997636@qq.com>
2018-06-26 16:15:34 +0800

    处理与index.vue中css的nav冲突

--- src/views/auth/personalcenter.vue
+++ src/views/auth/personalcenter.vue
@@ -1,6 +1,13 @@
 <style lang="css">
   @import "./css/base.css";
   @import "./css/personal.css";
+  nav {
+	display: inline-block;
+	margin: 60px auto 45px;
+	background-color: #5597b4;
+	box-shadow: 0 1px 1px #ccc;
+	border-radius: 2px;
+  }
 </style>
 
 <template>
